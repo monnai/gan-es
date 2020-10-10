d696e40f39ed8b4fe63af046155d5119e0fd2064
monnai <869625780@qq.com>
2020-10-10 10:17:59 +0800

    新建springboot项目，配置ignore文件

---
+++ src/main/java/com/gu/ganes/GanEsApplication.java
@@ -0,0 +1,13 @@
+package com.gu.ganes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanEsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GanEsApplication.class, args);
+  }
+
+}
